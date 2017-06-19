72b745691845a9f60b8862fbc92085f87b3e879f
Brettink <brett_wad_12@hotmail.com>
2017-06-19 16:31:02 -0600

    Stage 1

--- src/Project_Freedom.java
+++ src/Project_Freedom.java
@@ -1,16 +1,37 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JWindow;
 
 public class Project_Freedom extends JWindow{
 	
+	static Project_Freedom mainGame = null;
+	
+	int RESOLUTION_WIDTH = 1080;
+	int RESOLUTION_HEIGHT = 720;
+	static int SYSTEM_RES_WIDTH = 0;
+	static int SYSTEM_RES_HEIGHT = 0;
+	
 	Project_Freedom(){
+		positionWindowAndSize();
+		
 		setName("Project Freedom");
-		setLocation(0, 0);
-		setSize(1080, 720);
 		setVisible(true);
+	}
+	
+	public void positionWindowAndSize(){
+		setSize(RESOLUTION_WIDTH, RESOLUTION_HEIGHT);
+		
+		int posX = (SYSTEM_RES_WIDTH / 2) - (RESOLUTION_WIDTH / 2);
+		int posY = (SYSTEM_RES_HEIGHT / 2) - (RESOLUTION_HEIGHT / 2);
+		setLocation(posX, posY);
 	}
 
 	public static void main(String[] args) {
-		new Project_Freedom();
+		Dimension system_resolution = Toolkit.getDefaultToolkit().getScreenSize();
+		SYSTEM_RES_WIDTH = system_resolution.width;
+		SYSTEM_RES_HEIGHT = system_resolution.height;
+		mainGame = new Project_Freedom();
 
 	}
 
