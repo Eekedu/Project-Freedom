710988f6e0c928406bea1bf4700e9b330ba30a52
Brettink <brett_wad_12@hotmail.com>
2017-06-19 16:05:39 -0600

    Begin

---
+++ src/Project_Freedom.java
@@ -0,0 +1,17 @@
+import javax.swing.JWindow;
+
+public class Project_Freedom extends JWindow{
+	
+	Project_Freedom(){
+		setName("Project Freedom");
+		setLocation(0, 0);
+		setSize(1080, 720);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Project_Freedom();
+
+	}
+
+}
