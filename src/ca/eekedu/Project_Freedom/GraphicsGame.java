7505504de72f76c9dfb81eb0be6b4764f5deadfb
Brettink <brett_wad_12@hotmail.com>
2017-06-20 08:51:40 -0600

    Stage 1.3

--- src/ca/eekedu/Project_Freedom/GraphicsGame.java
+++ src/ca/eekedu/Project_Freedom/GraphicsGame.java
@@ -1,14 +1,31 @@
 package ca.eekedu.Project_Freedom;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 public class GraphicsGame extends JPanel {
+	int x = 0; int y = 0;
+	GraphicsGame(){
+		setBackground(new Color(255,255,255,0));
+	}
 	
 	private static final long serialVersionUID = -5342794367022521148L;
 
-	protected void paintComponent(java.awt.Graphics g){
+	public void update(){
+		repaint();
+	}
+	
+
+	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.drawLine(0, 0, 100, 100);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(new Color(75,75,75,255));
+		g2.drawRect(0, 0, MainGame.RESOLUTION_WIDTH - 1, MainGame.RESOLUTION_HEIGHT - 1);
+		//g.translate(x, y);
+		g2.fillRoundRect(x, y, 100, 100, 25, 25);
 	}
 
 }
--- src/ca/eekedu/Project_Freedom/MainGame.java
+++ src/ca/eekedu/Project_Freedom/MainGame.java
@@ -1,10 +1,15 @@
 package ca.eekedu.Project_Freedom;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
 import java.util.Map;
+
+
 import javax.swing.JFrame;
 
 public class MainGame extends JFrame{
@@ -12,12 +17,13 @@ public class MainGame extends JFrame{
 	private static final long serialVersionUID = -2787039850560314750L;
 
 	public static MainGame mainGame = null;
-	static GraphicsGame graphics = new GraphicsGame();
+	public static GraphicsGame graphics = new GraphicsGame();
+	public boolean enabled = true;
 	
 	Map<Integer, Integer> keysPressed = new HashMap<Integer, Integer>();
 	
-	int RESOLUTION_WIDTH = 1080;
-	int RESOLUTION_HEIGHT = 720;
+	static int RESOLUTION_WIDTH = 1080;
+	static int RESOLUTION_HEIGHT = 720;
 	static int SYSTEM_RES_WIDTH = 0;
 	static int SYSTEM_RES_HEIGHT = 0;
 	
@@ -26,7 +32,6 @@ public class MainGame extends JFrame{
 		
 		setTitle("Project Freedom");
 		setUndecorated(true);
-		setOpacity(0.75F);
 		addKeyListener(new KeyListener() {
 			
 			public void keyTyped(KeyEvent e) {}
@@ -43,21 +48,53 @@ public class MainGame extends JFrame{
 				}
 				if (e.getKeyCode() == KeyEvent.VK_ESCAPE){
 					dispose();
-				} else if (e.getKeyCode() == KeyEvent.VK_UP && keysPressed.containsKey(new Integer(KeyEvent.VK_SHIFT))){
-					RESOLUTION_WIDTH = 1280;
-					RESOLUTION_HEIGHT = 800;
-					positionWindowAndSize();
-				} else if (e.getKeyCode() == KeyEvent.VK_DOWN && keysPressed.containsKey(new Integer(KeyEvent.VK_SHIFT))){
-					RESOLUTION_WIDTH = 1080;
-					RESOLUTION_HEIGHT = 720;
-					positionWindowAndSize();
+					enabled = false;
+				}else if ( keysPressed.containsKey(new Integer(KeyEvent.VK_SHIFT))){
+					if (e.getKeyCode() == KeyEvent.VK_UP){
+						RESOLUTION_WIDTH = 1280;
+						RESOLUTION_HEIGHT = 800;
+						positionWindowAndSize();
+					} else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+						RESOLUTION_WIDTH = 1080;
+						RESOLUTION_HEIGHT = 720;
+						positionWindowAndSize();
+					}
+				}  else if (e.getKeyCode() == KeyEvent.VK_UP){
+					graphics.y-=10;
+				}  else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+					graphics.y+=10;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_LEFT){
+					graphics.x-=10;
+				}  else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+					graphics.x+=10;
 				}
 			}
 		});
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+		setBackground(new Color(255,255,255,0));
 		add(graphics);
 		setVisible(true);
+		
+		ActionListener firstTim = new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				remove(graphics);
+			}
+		};
+		
+		ActionListener secondTim = new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				add(graphics);
+				
+			}
+		};
+		
+		
+		while (enabled){
+			graphics.update();
+		}
 	}
 	
 	public void positionWindowAndSize(){
@@ -67,6 +104,7 @@ public class MainGame extends JFrame{
 		int posY = (SYSTEM_RES_HEIGHT / 2) - (RESOLUTION_HEIGHT / 2);
 		setLocation(posX, posY);
 	}
+	
 
 	public static void main(String[] args) {
 		Dimension system_resolution = Toolkit.getDefaultToolkit().getScreenSize();
@@ -75,5 +113,6 @@ public class MainGame extends JFrame{
 		mainGame = new MainGame();
 
 	}
+	
 
 }
